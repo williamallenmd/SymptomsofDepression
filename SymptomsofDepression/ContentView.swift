0ae4bc0d46de973dd71690715c001a9f86078517
William Allen <williamallenmd@gmail.com>
2022-07-01 17:46:57 -0700

    Initial Commit

---
+++ SymptomsofDepression/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SymptomsofDepression
+//
+//  Created by William Allen on 7/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SymptomsofDepression/SymptomsofDepressionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymptomsofDepressionApp.swift
+//  SymptomsofDepression
+//
+//  Created by William Allen on 7/1/22.
+//
+
+import SwiftUI
+
+@main
+struct SymptomsofDepressionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
